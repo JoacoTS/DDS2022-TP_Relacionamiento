c61246f1eb64e4edb4a2c32e7ba83f9299ece780
Alejo Lovallo <alejoe.lovallo@gmail.com>
2022-10-21 20:59:38 -0300

    feat(spark): setup done. Setting example and router

---
+++ src/main/java/Server/Server.java
@@ -0,0 +1,10 @@
+package Server;
+
+import static spark.Spark.*;
+
+public class Server {
+  public static void main(String[] args) {
+    port(9000);
+    get("/hello", (req, res) -> "Hello World");
+  }
+}
